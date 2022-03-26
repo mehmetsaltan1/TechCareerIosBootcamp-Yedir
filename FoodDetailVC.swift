628ac558edd42d9d6737f121fb5d5dbd762843e4
Mehmet Saltan <44650542+mehmetsaltan1@users.noreply.github.com>
2022-03-26 16:44:29 +0300

    Cart,Food Detail page vc add project

---
+++ FoodDetailVC.swift
@@ -0,0 +1,19 @@
+//
+//  FoodDetailVC.swift
+//  
+//
+//  Created by Mehmet Saltan on 26.03.2022.
+//
+
+import UIKit
+
+class FoodDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
---
+++ Yedir/cart-page-module/view/CartPageVC.swift
@@ -0,0 +1,19 @@
+//
+//  CartPageVC.swift
+//  
+//
+//  Created by Mehmet Saltan on 26.03.2022.
+//
+
+import UIKit
+
+class CartPageVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
